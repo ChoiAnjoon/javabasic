b7d974ce2e720d7972e6a01c335591cdfa7d4c5b
choi <dkswnsl12@naver.com>
2023-03-23 22:42:33 +0900

    java variables

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNo = "상암03";
+        int time = 3;
+        double distance = 1.5;
+
+        System.out.println("버스 번호는 " + busNo + "입니다.");
+        System.out.println("남은 시간은 " + time + "분 입니다.");
+        System.out.println("남은 거리는 " + distance + "km 입니다.");
+    }
+}
